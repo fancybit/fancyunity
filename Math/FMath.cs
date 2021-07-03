94e0c0e501dbfc8e7bbe34e7a4c6469a7b38ab88
fancbit <fancybit@qq.com>
2021-07-03 18:44:05 +0800

    添加射线到屏幕的投影点计算

---
+++ Math/FMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FancyUnity
+{
+    public class FMath
+    {
+        public static Vector3 RayPlaneProj(Vector3 point, Vector3 direct,
+            Vector3 planePoint, Vector3 planeNormal)
+        {
+            var distance = planePoint - point;
+            float d = Vector3.Dot(distance, planeNormal) / Vector3.Dot(direct, planeNormal);
+            direct *= d;
+            return direct + point;
+        }
+
+        public static Vector3 RayPlaneProj(Ray ray,Vector3 planePoint, Vector3 planeNormal)
+        {
+            return RayPlaneProj(ray.origin, ray.direction,planePoint,planeNormal);
+        }
+    }
+}
