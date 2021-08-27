c218bf5fac0d6f8cff2cbdb9fb52a26cd20a28ed
fancybit <fancybit@qq.com>
2021-08-27 22:54:28 +0800

    添加网络组件

---
+++ Network/NetMgr.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace FancyUnity
+{
+    public class NetMgr<T> : Singleton<T> where T : Component
+    {
+        public void Get<TRespData>(string url, Action<TRespData> callback)
+            => StartCoroutine(GetRequest(url, callback));
+
+        public IEnumerator GetRequest<TRespData>(string url, Action<TRespData> callback)
+        {
+            using (var webRequest = UnityWebRequest.Get(url))
+            {
+                var req = webRequest.SendWebRequest();
+                yield return req;
+
+                // 出现网络错误
+                if (webRequest.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError(webRequest.error + "\n" + webRequest.downloadHandler.text);
+                }
+                else// 正常处理
+                {
+                    // 获取到的数据
+                    string strData = webRequest.downloadHandler.text;
+                    var respData = JsonConvert.DeserializeObject<TRespData>(strData);
+                    callback?.Invoke(respData);
+                }
+            }
+        }
+
+        public void Post<TRespData, TReqData>(string url, TReqData postData,
+            Action<TRespData> callback)
+            => StartCoroutine(PostRequest(url, postData, callback));
+
+        public IEnumerator PostRequest<TRespData, TReqData>(
+            string url, TReqData postData, Action<TRespData> callback)
+        {
+            var jsonData = JsonConvert.SerializeObject(postData);
+            using (var webRequest = UnityWebRequest.Post(url, jsonData))
+            {
+                var req = webRequest.SendWebRequest();
+                yield return req;
+
+                // 出现网络错误
+                if (webRequest.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError(webRequest.error + "\n" + webRequest.downloadHandler.text);
+                }
+                else// 正常处理
+                {
+                    // 获取到的数据
+                    string strData = webRequest.downloadHandler.text;
+                    var respData = JsonConvert.DeserializeObject<TRespData>(strData);
+                    callback?.Invoke(respData);
+                }
+            }
+        }
+    }
+}
